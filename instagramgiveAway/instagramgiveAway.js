4e16911a334982ec813dfe7b9a889e75c846fef5
pufick <98830871+pufick@users.noreply.github.com>
2022-10-02 14:04:18 +0300

    Update instagramgiveAway.js

--- instagramgiveAway/instagramgiveAway.js
+++ instagramgiveAway/instagramgiveAway.js
@@ -1,29 +1,25 @@
-import {Node,LinkedList} from './LinkedList.js'
-import {GetHash,HashTable} from './HashTable.js'
-import {readFileSync} from 'fs'
+import {HashTable} from './HashTable.js'
+import {readFileSync, readdirSync, statSync} from 'fs'
+import path from 'path';
+import { fileURLToPath } from 'url';
 let hashTable = new HashTable()
-function runFile(file){
-  let data
-    try {
-      data = readFileSync(file, "utf8")
-    } catch (err) {
-      console.error(err);
-    }
-    let array = data.split("\n")
-    array = new Set(array)
-    for (let i of array){
-      hashTable.insert(i)
-    }
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const files = readdirSync(__dirname+"/files")
+files.forEach((element, index) => {files[index] = __dirname+"/files/"+element})
+
+let setOfWordsInOneFile
+for(let file of files){
+try {
+  setOfWordsInOneFile = new Set(readFileSync(file, "utf8").split("\n"))
+} catch (err) {
+  console.error(err);
+}
+for (let word of setOfWordsInOneFile){
+  hashTable.insert(word)
+}
 }
 
-  let file_ = './files\\out'
-  
- for (let i = 0; i < 20; i++){
-let file = file_ + i + ".txt"
-console.log(file)
-runFile(file)
-}
-
-//console.log(hashTable.existInAllFiles())//441
-//console.log(hashTable.existInAtLeastTen())//73245
-
+console.log(hashTable.existInAllFiles())//441
+console.log(hashTable.existInAtLeastTen())//73245
+console.log(hashTable.uniqueValues())

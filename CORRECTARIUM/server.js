33690557b558eda4fa8e2567d87d1fb81dd62243
pufick <98830871+pufick@users.noreply.github.com>
2022-12-26 14:32:37 +0200

    Create server.js

---
+++ CORRECTARIUM/server.js
@@ -0,0 +1,28 @@
+import bodyParser from 'body-parser'
+import express from 'express'
+import { GetPrice,
+    GetDeadline,
+    GetWorkingTime} from './index.js'
+    import {Language} from './classes.js'
+    const Ukrainian = new Language(0.05, 50, 1333)
+    const English = new Language(0.12, 120, 333)
+    const Russian = new Language(0.05, 50, 1333)
+    const languages = {'ukr': Ukrainian, 'en': English, 'rus': Russian}
+const app = express()
+const PORT = 3000
+
+app.listen(PORT)
+app.use(bodyParser.json())
+app.post('/', (req, res) => {
+    const language = languages[req.body.language]
+    const extension = req.body.mimetype
+    const symbolsCount = req.body.count
+    const workingTime = GetWorkingTime(language, symbolsCount, extension)
+    const price = GetPrice(language, symbolsCount, extension)
+    const deadline = GetDeadline(workingTime, new Date())
+    res.status(200).send({"price": price.toFixed(2),
+    "time": workingTime.toFixed(2),
+    "deadline": Math.floor(deadline.getTime() / 1000),
+    "deadline_date": deadline.toLocaleString()
+})
+})

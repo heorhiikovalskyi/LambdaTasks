393d4961d7494a63a888f7cd0bf36df0d362de85
pufick <98830871+pufick@users.noreply.github.com>
2022-12-26 14:30:43 +0200

    Create classes.js

---
+++ CORRECTARIUM/classes.js
@@ -0,0 +1,10 @@
+class Language{
+    constructor(onesymbolPrice, minimumPrice, hourVelocity){
+    this.onesymbolPrice = onesymbolPrice
+    this.minimumPrice = minimumPrice
+    this.hourVelocity = hourVelocity
+    }
+}
+export{
+    Language
+}

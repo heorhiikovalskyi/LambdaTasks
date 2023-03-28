19d7e96a97b0a9fee58458b063a38b931495b0e5
pufick <98830871+pufick@users.noreply.github.com>
2023-03-28 20:51:27 +0300

    modified:   CLITelegramEcho/index.js 	modified:   CLITelegramEcho/package-lock.json 	modified:   CLITelegramEcho/package.json

--- CLITelegramEcho/index.js
+++ CLITelegramEcho/index.js
@@ -1,29 +1,28 @@
-import axios from 'axios';
-import TelegramBot from 'node-telegram-bot-api';
-const TOKEN = '5784622230:AAGuSeRxIDcQhYuJOC0LEIGoUfYBlGLupNk'
-const CHAT_ID = '462675613'
-const URL = 'https://picsum.photos/200/300' 
-const api = new TelegramBot(
-    TOKEN,
-    {polling: true},
-)
+import "dotenv/config.js";
+import axios from "axios";
+import TelegramBot from "node-telegram-bot-api";
+const TOKEN = process.env.TOKEN;
+const CHAT_ID = process.env.CHAT_ID;
+const randPictureEndpoint = process.env.RAND_PICTURE_ENDPOINT;
+const api = new TelegramBot(TOKEN, { polling: true });
 
-console.log("Telegram bot ready to work")
+console.log("Telegram bot ready to work");
 
-api.on('message', (msg) => {
-if (msg.text === 'photo'){
-axios.get(URL)
-.then(async function(response){
-    await api.sendPhoto(CHAT_ID, response.request._redirectable._options.href)
-    console.log(`${msg.from.first_name} got an image`)
-})
-.catch(function(error){
-    console.log(error)
-})
-}
-else{
-const response = `${msg.from.first_name} send message \'${msg.text}\'`
-console.log(response)
-api.sendMessage(CHAT_ID, response)
-}
-})
+api.on("message", (msg) => {
+  if (msg.text === "photo") {
+    axios
+      .get(randPictureEndpoint)
+      .then(async function (picture) {
+        const pictureLink = picture.request._redirectable._options.href;
+        await api.sendPhoto(CHAT_ID, pictureLink);
+        console.log(`${msg.from.first_name} got an image`);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } else {
+    const response = `${msg.from.first_name} send message \'${msg.text}\'`;
+    console.log(response);
+    api.sendMessage(CHAT_ID, response);
+  }
+});

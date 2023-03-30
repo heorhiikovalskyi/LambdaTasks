9fd4dee659c77dfb5b5b3bb46012df3915706c27
pufick <98830871+pufick@users.noreply.github.com>
2023-03-30 10:43:12 +0300

    modified:   JSONstorage/package.json 	modified:   JSONstorage/src/DBConnect.ts

--- JSONstorage/src/DBConnect.ts
+++ JSONstorage/src/DBConnect.ts
@@ -1,11 +1,9 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient } from "mongodb";
 
-async function ConnectToDB(url: string){
-    const client = new MongoClient(url);
-    await client.connect()
-    console.log('Connected successfully to MongoDB server');
-  }
-
-export {
-    ConnectToDB
+async function ConnectToDB(url: string) {
+  const client = new MongoClient(url);
+  await client.connect();
+  console.log("Connected successfully to MongoDB server");
 }
+
+export { ConnectToDB };

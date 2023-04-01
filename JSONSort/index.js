28fd81ff97eaf84e0ad5ddeea96703e1afc44858
pufick <98830871+pufick@users.noreply.github.com>
2023-04-01 21:11:22 +0300

    modified:   JSONSort/index.js

--- JSONSort/index.js
+++ JSONSort/index.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 import { readFileSync } from "fs";
-const truefalseCount = { trues: 0, falses: 0 };
-const endpoints = readFileSync("./endpoints.txt", "utf8").split("\n");
+const trueFalseCount = { trues: 0, falses: 0 };
+const endpoints = readFileSync("./endpoints.txt", "utf8")
+  .split("\n")
+  .filter((endpoint) => endpoint);
 
-function JSONParse(objectToParse) {
-  const keys = Object.keys(objectToParse);
+function isDoneSearch(objectToSearch) {
+  const keys = Object.keys(objectToSearch);
   for (let key of keys) {
-    const value = objectToParse[key];
+    const value = objectToSearch[key];
     if (typeof value === "object") {
-      const isDone = JSONParse(value);
+      const isDone = isDoneSearch(value);
       if (isDone === true || isDone === false) return isDone;
     }
     if (key === "isDone") return value;
@@ -16,25 +18,33 @@ function JSONParse(objectToParse) {
   return undefined;
 }
 
-let counter = 0; //для умови з трьома звертаннями на endpoint
-for (let i = 0; i < endpoints.length; i++) {
-  let JSON;
-  try {
-    JSON = (await axios.get(endpoints[i])).data;
-  } catch (error) {
-    counter += 1;
-    if (counter === 3) {
-      console.log(error.message);
-      counter = 0;
-    } else i -= 1;
-    continue;
-  }
-  console.log(endpoints[i]);
-  const isDone = JSONParse(JSON);
+const getJson = (endpoint) => {
+  return new Promise(async (resolve, reject) => {
+    let counter = 0;
+    let JSON;
+    while (counter !== 3) {
+      try {
+        JSON = (await axios.get(endpoint)).data;
+        return resolve({ endpoint: endpoint, JSON: JSON });
+      } catch (err) {
+        counter += 1;
+      }
+    }
+    return resolve();
+  });
+};
+
+const getJsonArray = [];
+endpoints.forEach((endpoint) => getJsonArray.push(getJson(endpoint)));
+const endpointsJsons = await Promise.all(getJsonArray);
+endpointsJsons.forEach((endpointJson) => {
+  if (!endpointJson) return;
+  const { endpoint, JSON } = endpointJson;
+  console.log(endpoint);
+  const isDone = isDoneSearch(JSON);
   console.log(isDone);
-  if (isDone === false) truefalseCount["falses"] += 1;
-  else truefalseCount["trues"] += 1;
-  counter = 0;
-}
-console.log(`Trues: ${truefalseCount["trues"]}`);
-console.log(`Falses: ${truefalseCount["falses"]}`);
+  isDone ? (trueFalseCount.trues += 1) : (trueFalseCount.falses += 1);
+});
+
+console.log(`Trues: ${trueFalseCount.trues}`);
+console.log(`Falses: ${trueFalseCount.falses}`);
